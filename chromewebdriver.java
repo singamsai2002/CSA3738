0bc83a2fdcde2eff203092d017e44a06dda9cdf9
singamsai2002 <112846532+singamsai2002@users.noreply.github.com>
2023-06-21 06:39:55 +0530

    Create chromewebdriver.java

---
+++ chromewebdriver.java
@@ -0,0 +1,15 @@
+package ame;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class CHROME{
+public static void main(String[] args) {
+System.out.println("hai");
+System.setProperty("webdriver.chrome.driver","C:\\\\chromedriver_win32\\chromedri
+ver.exe");
+WebDriver driver = new ChromeDriver();
+driver.manage().window().maximize();
+driver.get("https://www.google.com/");
+}
+}
